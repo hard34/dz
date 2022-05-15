b5b821a70cae71a5545c40647e4cda4949c2ba82
hard34 <derbin398@gmail.com>
2022-05-15 21:41:43 +0100

    Дана последовательность целых чисел. Удалить все минимальные элементы.

--- I&P/Work_with_STL_library/task1_2_3gr.cpp
+++ I&P/Work_with_STL_library/task1_2_3gr.cpp
@@ -11,23 +11,23 @@ int main() {
 	vector<int> vec;
 
 	int x;
-	for (int i = 0; i < n; i++) {//создание вектора
+	for (int i = 0; i < n; i++) {
 		cout << "a[" << i << "] = ";
 		cin >> x;
 		vec.push_back(x);
 	}
 
-	for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++)//вывод
+	for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++)
 		cout << *iter << " ";
 	cout << endl;
 
-	cout << " Удаление минимальные элементов:\n"; 
-	vector<int>::iterator iter = min_element(vec.begin(), vec.end()); //поиск минимальных элементов
+	cout << "Remove all minimal elements:\n"; 
+	vector<int>::iterator iter = min_element(vec.begin(), vec.end()); 
 	int Min = *iter;
 	vector<int>::iterator it = remove(vec.begin(), vec.end(), Min);
-	vec.erase(it, vec.end()); //удаление
+	vec.erase(it, vec.end());
 	
-	for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++) //вывод
+	for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++) 
 		cout << *iter << " ";
 	cout << endl;
 	return 0;
