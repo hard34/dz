d36e5d8efdbf5f93298ba270a5ed278a77c333fe
hard34 <derbin398@gmail.com>
2022-05-15 21:52:51 +0100

    Даны две последовательности целых чисел. В первой последовательности удалить все элементы, кратные X. Во второй — заменить элементы, попадающие в интервал [a, b] числом Y. Отсортировать данные последовательности. Объединить их в одну отсортированную последовательность.

--- I&P/Work_with_STL_library/task1_12_3gr.cpp
+++ I&P/Work_with_STL_library/task1_12_3gr.cpp
@@ -4,12 +4,12 @@
 #include<numeric>
 using namespace std;
 
-int X;  //предикат может содержать только один параметр
-bool odd(int x) {//кратные Х
+int X;  
+bool odd(int x) {
 	return x % X == 0;
 }
 int a, b;
-bool ab(int x) {//промежуток
+bool ab(int x) {
 	return (x >= a && x <= b);
 }
 
@@ -22,60 +22,60 @@ int main() {
 	vector<int> vec, vec1, vec2;
 
 	int x;
-	for (int i = 0; i < n; i++) {//создание вектора
+	for (int i = 0; i < n; i++) {
 		cout << "a[" << i << "] = ";
 		cin >> x;
 		vec1.push_back(x);
 	}
-	for (vector<int>::iterator iter = vec1.begin(); iter != vec1.end(); iter++)//вывод
+	for (vector<int>::iterator iter = vec1.begin(); iter != vec1.end(); iter++)
 		cout << *iter << " ";
 	cout << endl;
 
 	cout << "m = "; cin >> m;
-	for (int i = 0; i < m; i++) {//создание вектора
+	for (int i = 0; i < m; i++) {
 		cout << "a[" << i << "] = ";
 		cin >> x;
 		vec2.push_back(x);
 	}
 
-	for (vector<int>::iterator iter = vec2.begin(); iter != vec2.end(); iter++)//вывод
-		cout << *iter << " ";
-	cout << endl;
-
-	cout << "X = ";	cin >> X;
-	cout << " Удаление кратные X элементов:\n";
-	vector<int>::iterator it = remove_if(vec1.begin(), vec1.end(), odd);
-	vec1.erase(it, vec1.end()); //удаление
-
-	for (vector<int>::iterator iter = vec1.begin(); iter != vec1.end(); iter++) //вывод
-		cout << *iter << " ";
-	cout << endl;
-
-	int Y;
-	cout << "Введите диапозон:\n";
-	cin >> a >> b;
-	cout << "Введите Y:\n";
-	cin >> Y;
-	cout << "Заменить элементы, попадающие в интервал [a,b] числом Y:\n";
-	replace_if(vec2.begin(), vec2.end(), ab, Y); //замена
-
-	for (vector<int>::iterator iter = vec2.begin(); iter != vec2.end(); iter++) //вывод
-		cout << *iter << " ";
-	cout << endl;
-
-	cout << "Сортировка1:\n";
-	sort(vec1.begin(), vec1.end()); //сортировка
-	for (vector<int>::iterator iter = vec1.begin(); iter != vec1.end(); iter++)
-		cout << *iter << " ";
-	cout << endl;
-
-	cout << "Сортировка2:\n";
-	sort(vec2.begin(), vec2.end()); //сортировка
 	for (vector<int>::iterator iter = vec2.begin(); iter != vec2.end(); iter++)
 		cout << *iter << " ";
 	cout << endl;
 
-	cout << "Объединение:\n"; //объединяет
+	cout << "X = ";	cin >> X;
+	cout << "In the first sequence, delete all elements that are multiples of X.:\n";
+	vector<int>::iterator it = remove_if(vec1.begin(), vec1.end(), odd);
+	vec1.erase(it, vec1.end()); 
+
+	for (vector<int>::iterator iter = vec1.begin(); iter != vec1.end(); iter++) 
+		cout << *iter << " ";
+	cout << endl;
+
+	int Y;
+	cout << "Enter the gap:\n";
+	cin >> a >> b;
+	cout << "Enter Y:\n";
+	cin >> Y;
+	cout << "In the second one, replace the elements falling in the interval [a, b] with the number Y:\n";
+	replace_if(vec2.begin(), vec2.end(), ab, Y); 
+
+	for (vector<int>::iterator iter = vec2.begin(); iter != vec2.end(); iter++) 
+		cout << *iter << " ";
+	cout << endl;
+
+	cout << "Sort vector 1:\n";
+	sort(vec1.begin(), vec1.end()); 
+	for (vector<int>::iterator iter = vec1.begin(); iter != vec1.end(); iter++)
+		cout << *iter << " ";
+	cout << endl;
+
+	cout << "Sort vector 2:\n";
+	sort(vec2.begin(), vec2.end()); 
+	for (vector<int>::iterator iter = vec2.begin(); iter != vec2.end(); iter++)
+		cout << *iter << " ";
+	cout << endl;
+
+	cout << "Combine them into one:\n"; 
 	vec.insert(vec.end(), vec1.begin(), vec1.end());
 	vec.insert(vec.end(), vec2.begin(), vec2.end());
 
@@ -83,8 +83,8 @@ int main() {
 		cout << *iter << " ";
 	cout << endl;
 
-	cout << "Сортировка:\n";
-	sort(vec.begin(), vec.end()); //сортировка
+	cout << "Sort vector:\n";
+	sort(vec.begin(), vec.end()); 
 	for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++)
 		cout << *iter << " ";
 	cout << endl;
