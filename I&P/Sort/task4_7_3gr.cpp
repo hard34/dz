672a9e6aaafbc1d02388cce34147712a30a25895
hard34 <derbin398@gmail.com>
2022-05-16 17:37:14 +0100

    Четные строки по убыванию, нечетные по возрастанию с помощью сортировки Шелла с шагом d = 3^i - 1.

---
+++ I&P/Sort/task4_7_3gr.cpp
@@ -0,0 +1,79 @@
+# include <iostream>
+# include <fstream>
+# include <cstdio>
+# include <string>
+# include <vector>
+# include <algorithm>
+
+
+using namespace std;
+
+ifstream in("input.txt");  
+ofstream out("output.txt");  
+
+
+vector<vector<int>> create() 
+{
+	int n;
+	string count;
+	in >> count;
+	n = stoi(count);
+	vector<vector<int>> matrix;
+	for (int i = 0; i < n; i++) {
+		vector<int> tempVec;
+		for (int j = 0; j < n; j++) {
+			string temp;
+			in >> temp;
+			int t = stoi(temp);
+			if (i % 2 != 0) {
+				t *= (-1);
+			}
+			tempVec.push_back(t);
+		}
+		matrix.push_back(tempVec);
+	}
+	return matrix;
+}
+
+vector<int> shellSort(vector<int> a) {
+	int step;
+	int temp = 1;
+	while (pow(3, temp) - 1 < a.size() - 1) {
+		temp++;
+	}
+	step = pow(3, temp) - 1;
+	while (step > 0) {
+		for (int i = 0; i < a.size() - step; i++) {
+			int j = i;
+			while ((j >= 0) && (a[j] > a[j + step])) {
+				swap(a[j], a[j + step]);
+				j -= step;
+			}
+		}
+		temp--;
+		step = pow(3, temp) - 1;
+		if (temp == 0) {
+			step++;
+		}
+	}
+	return a;
+}
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	vector <vector<int>> matrix = create();
+	for (int i = 0; i < matrix.size(); i++) {
+		matrix[i] = shellSort(matrix[i]);
+	}
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix.size(); j++) {
+			if (i % 2 != 0) {
+				matrix[i][j] *= (-1);
+			}
+			cout << matrix[i][j] << ' ';
+		}
+		cout << "\n";
+	}
+	system("pause");
+	return 0;
+}
