19c4ef3f3399f8ab268f8cfdc6dd6b391137c1ab
hard34 <derbin398@gmail.com>
2022-05-17 16:19:52 +0100

    Диагонали, параллельные побочной по возрастанию с помощью сортировки пузырьком.

--- I&P/Sort/task4_11_3gr.cpp
+++ I&P/Sort/task4_11_3gr.cpp
@@ -1,68 +1,123 @@
-#include <fstream>
 #include <iostream>
-#include <iomanip>
+#include <fstream>
+#include <string>
 using namespace std;
 
-ifstream in("input_task4.txt");
-ofstream out("output.txt");
-
-void sort(int* a, int n)
-{
-    int a1, b1, a2, b2;
-    bool chk;
-    for (int j = 1; j < n - 1; ++j)
-    {
-        do {
-            chk = false;
-            for (int i = j; i >= 1; --i)
-            {
-                if (a[i][j - i] < a[i - 1][j - (i - 1)])
-                {
-                    swap(a[i][j - i], a[i - 1][j - (i - 1)]);
-                    chk = true;
-                }
-            }
-        } while (chk);
+int num_lines(string file_name) { //подсчёт строк в массиве
+    ifstream file(file_name);
+    string line;
+    int result = 0;
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            result++;
+        }
     }
-
-    for (int j = 1; j < n; ++j)
-    {
-        do {
-            chk = false;
-            for (int i = j; i >= 1; --i)
-            {
-                a1 = n - 1 - j + (i - 1);
-                b1 = n - 1 - (i - 1);
-
-                a2 = n - 1 - j + i;
-                b2 = n - 1 - i;
-                if (a[a1][b1] > a[a2][b2])
-                {
-                    swap(a[a1][b1], a[a2][b2]);
-                    chk = true;
-                }
-            }
-        } while (chk);
-    }
+    file.close();
+    return result;
 }
 
-int main(void)
-{
-    int n, m, i, j,
-        int a[8][8];
-    cin >> n >> m;
-    for (i = 0; i < n; ++i)
-        for (j = 0; j < m; ++j)
-            cin >> a[i][j];
-    sort(a);
-
-    cout << n << "\t" << m << "\n";
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-            cout << setw(5) << a[i][j];
-        cout << "\n";
+int** import_from_file(string file_name) { //импорт массива из файла
+    int n = num_lines(file_name);
+    ifstream file(file_name);
+    int** result = new int* [n];
+    for (int i = 0; i < n; i++) {
+        result[i] = new int[n];
     }
-    in.close(); out.close();
-    return 0;
+    string line;
+    if (file.is_open()) {
+        int col = 0;
+        int j = 0;
+        while (getline(file, line)) {
+            string data = line;
+            int num;
+            string now_num = "";
+                for (int i = 0; i < data.size(); i++) {
+                    if (data[i] == ' ') {
+                        num = stoi(now_num);
+                        now_num = "";
+                        result[col][j] = num;
+                        j++;
+                    }
+                    else {
+                        now_num += data[i];
+                    }
+                }
+            num = stoi(now_num);
+            now_num = "";
+            result[col][j] = num;
+            col++;
+            j = 0;
+        }
+    }
+    file.close();
+    return result;
+}
+
+void output_to_file(string file_name, int** arr, int n) {
+    ofstream file;
+    file.open(file_name);
+    if (file.is_open()) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                file « arr[i][j] « ' ';
+            }
+            file « endl;
+        }
+    }
+    file.close();
+}
+
+int* bubbleSort(int* d, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = n - 1; j >= i; j--) {
+            if (d[j - 1] > d[j]) {
+                int tmp = d[j - 1];
+                d[j - 1] = d[j];
+                d[j] = tmp;
+            }
+        }
+    }
+    return d;
+}
+
+int** bubbleSort_start(int** mas, int n) {
+    for (int i = 1; i < n; i++) {
+        int* a = new int[i + 1];
+            for (int j = 0; j <= i; j++) {
+                a[j] = mas[i - j][j];
+            }
+            a = bubbleSort(a, i + 1);
+        for (int j = 0; j <= i; j++) {
+            mas[i - j][j] = a[j];
+        }
+    delete[] a;
+}
+
+for (int j = 1; j < n - 1; j++) {
+    int* a = new int[n - j];
+    int k = 0;
+    for (int i = n - 1; i >= j; i--) {
+        a[k] = mas[i][k + j];
+        k++;
+    }
+    a = bubbleSort(a, n - j);
+    k = 0;
+    for (int i = n - 1; i >= j; i--) {
+        mas[i][k + j] = a[k];
+        k++;
+    }
+    delete[] a;
+}
+return mas;
+}
+
+int main(){
+    int size = num_lines("input.txt"); //n-мерность
+    int** array = new int* [size]; //инициализация массива
+    array = import_from_file("input.txt"); //массив, импортированный из файла
+    array = bubbleSort_start(array, size); //применение сортировки
+    output_to_file("output.txt", array, size);
+    for (int i = 0; i < size; i++) // удаление динамического массива из памяти
+        delete[] array[i];
+    delete[] array;
 }
